0467876e52ea96204a842aed9fea917568708dfb
doalperez12@gmail.com <doalperez12@gmail.com>
2023-10-20 10:43:45 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,39 +1,37 @@
-const navbar = document.querySelectorAll ('.nav-link');
 
-navLink.forEach(link => {
-  link.addEventListener('click', () => {
-    navLink.forEach(otherLink => otherLink.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
+function validarFormulario(){
+  
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const email = document.getElementById("email").value;
 
-const form = document.getElementById("form");
-const nombre = document.getElementById("nombre");
-const parrafo = document.getElementById("alertas");
+  /* if (nombre === "") {
+    alert("Por favor, ingresa tu nombre.");
+    return false;
+  } */
 
-function validarFormulario() {
-  let warnings = "";
-  let valido = true;
-  parrafo.innerHTML = "";
+  if(nombreUsuario === "" || nombreUsuario === null || nombreUsuario.lenght === 0)
+    {alert("Error: debe llenar este campo");
+    nombreUsuario.focus();
+    return false; }
 
-  if (nombre.value.length < 4) {
-    warnings += `El nombre debe contener más de 4 carcateres`;
-    valido = false;
+
+
+
+
+  if (apellido === "") {
+    alert("Por favor, ingresa tu nombre.");
+    return false;
+  }
+  if (email === "") {
+    alert("Por favor, ingresa tu correo electrónico.");
+    return false;
   }
 
-  if (!valido) {
-    parrafo.innerHTML = warnings;
-  } else {
-    parrafo.innerHTML = "Enviado";
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido.");
+    return false;
   }
-  return valido;
-}
 
-form.addEventListener("submit", (e) => {
-  if (validarFormulario()) {
-    // Si la validación es exitosa, puedes enviar el formulario
-    formulario.submit();
-  } else {
-    e.preventDefault(); // Evita que el formulario se envíe automáticamente
-  }
-});
+  return true;
+} 
